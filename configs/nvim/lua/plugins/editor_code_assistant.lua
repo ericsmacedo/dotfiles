2ce2c488c983749ea7259219e48c3e723006c7c5
emacedo <ericsmacedo@gmail.com>
2025-10-20 10:07:27 +0100

    Adds editor_code_assistant

---
+++ configs/nvim/lua/plugins/editor_code_assistant.lua
@@ -0,0 +1,10 @@
+-- Editor code assistant
+-- Integrates AI into neovim
+return {
+  "editor-code-assistant/eca-nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",   -- Required: UI framework
+    "nvim-lua/plenary.nvim",  -- Optional: Enhanced async operations
+  },
+  opts = {}
+}
